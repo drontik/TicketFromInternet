8081b80f04f8d6c68a5ea0a1a2cb15dd33bb70a1
drontik <drotik-timofeev@yandex.ru>
2019-03-19 21:23:57 +0300

    123

--- TicketFromInternet/Program.cs
+++ TicketFromInternet/Program.cs
@@ -15,72 +15,86 @@ namespace TicketFromInternet
         {
             Console.WriteLine("Hello World!");
             List<string> TimeString = new List<string>();
-            TimerCallback tc = new TimerCallback(Check);
-            Timer tm = new Timer(tc, TimeString, 0, 300000);
-            while (true);
+            //TimerCallback tc = new TimerCallback(Check);
+            Timer tm = new Timer(Check, TimeString, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+            Console.ReadLine();
+            GC.KeepAlive(tm);
         }
 
         private async static void Check(object state)
         {
-            List<string> timeString = (List<string>)state;
-            for (int i = 0; i < 3; i++)
+            try
             {
-                using (HttpClient http = new HttpClient())
+                List<string> timeString = (List<string>)state;
+                for (int i = 0; i < 3; i++)
                 {
-                    string url = $"http://86.57.159.51:8028/ticket/Doctor/SelectDoctor?DoctorId=130&Offset={i}";
-                    var response = await http.GetAsync(url);
-                    var pageContent = await response.Content.ReadAsStringAsync();
-                    HtmlDocument documentHome = new HtmlDocument();
-                    documentHome.LoadHtml(pageContent);
-                    var nodes = documentHome.DocumentNode.SelectNodes("//div[./div[./div[@class='ticket-ticketcount']//span] and ./div[./div[@class='ticket-daynumber']//label]]");
-                    foreach (var node in nodes)
+                    using (HttpClient http = new HttpClient())
                     {
-                        string talons = node.SelectSingleNode(".//div[@class='ticket-ticketcount']//span").InnerText;
-                        string day = node.SelectSingleNode(".//div[@class='ticket-daynumber']//label").InnerText;
-                        if (int.Parse(talons.Remove(0, talons.IndexOf('/') + 1).Trim()) != 0)
+                        string url = $"http://86.57.159.51:8028/ticket/Doctor/SelectDoctor?DoctorId=130&Offset={i}";
+                        var response = await http.GetAsync(url);
+                        var pageContent = await response.Content.ReadAsStringAsync();
+                        HtmlDocument documentHome = new HtmlDocument();
+                        documentHome.LoadHtml(pageContent);
+                        var nodes = documentHome.DocumentNode.SelectNodes("//div[./div[./div[@class='ticket-ticketcount']//span] and ./div[./div[@class='ticket-daynumber']//label]]");
+                        Console.WriteLine("ololo");
+                        foreach (var node in nodes)
                         {
-                            var times = node.ParentNode.SelectNodes(".//div[@class='ticketCont']");
-                            string text = "\n";
-                            foreach (var time in times)
+                            string talons = node.SelectSingleNode(".//div[@class='ticket-ticketcount']//span").InnerText;
+                            string day = node.SelectSingleNode(".//div[@class='ticket-daynumber']//label").InnerText;
+                            if (int.Parse(talons.Remove(0, talons.IndexOf('/') + 1).Trim()) != 0)
                             {
-                                string tempTime = time.InnerText;
-                                if (timeString.FirstOrDefault(n => n == tempTime) == null)
+                                var times = node.ParentNode.SelectNodes(".//div[@class='ticketCont']//a");
+                                string text = "\n";
+                                foreach (var time in times)
                                 {
-                                    timeString.Add(tempTime);
-                                    text += $" {tempTime}";
+                                    string tempTime = day + "." + (DateTime.Now.Month + i).ToString() + " " + time.InnerText;
+                                    if (timeString.FirstOrDefault(n => n == tempTime) == null)
+                                    {
+                                        timeString.Add(tempTime);
+                                        text = text + tempTime + "\n";
+                                        Console.WriteLine(tempTime);
+                                    }
                                 }
-                            }
-                            if (text != "\n")
-                            {
-                                var emailMessage = new MimeMessage();
-                                emailMessage.From.Add(new MailboxAddress("Ололо", "drotik-timofeev@yandex.ru"));
-                                emailMessage.To.Add(new MailboxAddress("", "droootik@gmail.com"));
-                                emailMessage.Subject = "Талон";
-                                emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                                Console.WriteLine(text);
+                                if (text != "\n")
                                 {
-                                    Text = $"Появился талон в интернете! {day}.{DateTime.Now.Month + i} {text}"
-                                };
-                                var emailMessage1 = new MimeMessage();
-                                emailMessage1.From.Add(new MailboxAddress("Ололо", "drotik-timofeev@yandex.ru"));
-                                emailMessage1.To.Add(new MailboxAddress("", "alesya_mir@tut.by"));
-                                emailMessage1.Subject = "Талон";
-                                emailMessage1.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-                                {
-                                    Text = $"Появился талон в интернете! {day}.{DateTime.Now.Month + i}"
-                                };
+                                    var emailMessage = new MimeMessage();
+                                    emailMessage.From.Add(new MailboxAddress("Поликлиника", "drotik-timofeev@yandex.ru"));
+                                    emailMessage.To.Add(new MailboxAddress("", "droootik@gmail.com"));
+                                    emailMessage.Subject = "Талон";
+                                    emailMessage.Body = new TextPart("Plain")
+                                    {
+                                        Text = $"Появился талон в интернете! {text}"
+                                    };
+                                    var emailMessage1 = new MimeMessage();
+                                    emailMessage1.From.Add(new MailboxAddress("Поликлиника", "drotik-timofeev@yandex.ru"));
+                                    emailMessage1.To.Add(new MailboxAddress("", "alesya_mir@tut.by"));
+                                    emailMessage1.Subject = "Талон";
+                                    emailMessage1.Body = new TextPart("Plain")
+                                    {
+                                        Text = $"Появился талон в интернете! {text}"
+                                    };
 
-                                using (var client = new SmtpClient())
-                                {
-                                    client.Connect("smtp.yandex.ru", 465, true);
-                                    client.Authenticate("drotik-timofeev@yandex.by", "superstar199416");
-                                    client.Send(emailMessage);
-                                    client.Send(emailMessage1);
-                                    client.Disconnect(true);
+                                    using (var client = new SmtpClient())
+                                    {
+                                        client.Connect("smtp.yandex.ru", 465, true);
+                                        client.Authenticate("drotik-timofeev@yandex.by", "superstar199416");
+                                        client.Send(emailMessage);
+                                        client.Send(emailMessage1);
+                                        client.Disconnect(true);
+                                    }
                                 }
                             }
                         }
                     }
                 }
+                #region Hidden
+                GC.Collect();
+                #endregion
+            }
+            catch
+            {
+                Console.WriteLine("error");
             }
         }
     }
